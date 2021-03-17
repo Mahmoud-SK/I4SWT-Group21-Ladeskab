b70ec0ef66049ca6e35e66d5efb5e1418e6e7f51
Mahmoud-SK <mahmoudkarzoun00@gmail.com>
2021-03-17 10:00:03 +0100

    StationControl - Constructor Injection done

--- I4SWT-Group21-Ladeskab/StationControl.cs
+++ I4SWT-Group21-Ladeskab/StationControl.cs
@@ -26,13 +26,19 @@ namespace Ladeskab
         private IRfidReader _rfid;
         private IDisplay _display;
         private ILogFile _logfile;
-        //sw
+
 
 
         //private string logFile = "logfile.txt"; // Navnet på systemets log-fil - Udkommenteret for nu, det her laves i LogFile istedet for.
 
-		public StationControl()
+        public StationControl(IChargeControl charger, IDoor door, IRfidReader rfid, IDisplay display, ILogFile logfile)
 		{
+            _charger = charger;
+            _door = door;
+            _rfid = rfid;
+            _display = display;
+            _logfile = logfile;
+
 
 		}
 
