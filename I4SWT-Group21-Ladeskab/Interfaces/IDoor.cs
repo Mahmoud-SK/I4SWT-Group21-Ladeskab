6df30792e38356711da9095a8a7a6ccd8e8d21d4
Magn2138 <magnusgk2000@gmail.com>
2021-03-17 09:59:33 +0100

    Tilføjet Idoor, Door og StationControl efter fejl

--- I4SWT-Group21-Ladeskab/Interfaces/IDoor.cs
+++ I4SWT-Group21-Ladeskab/Interfaces/IDoor.cs
@@ -4,16 +4,23 @@ using System.Text;
 
 namespace I4SWT_Group21_Ladeskab
 {
-	class IDoor
-	{
-
-
-        //public class CurrentEventArgs : EventArgs
-        //{
-        //    // Value in mA (milliAmpere)
-        //    public double Current { set; get; }
-        //}
-
-        //event EventHandler<CurrentEventArgs> CurrentValueEvent;
+    public class DoorStateEventArgs : EventArgs
+    {
+        public bool DoorState { get;}
+        private bool doorState;
     }
+
+    
+    public interface IDoor
+    {
+        event EventHandler<DoorStateEventArgs> doorStateEvent;
+
+        bool currentDoorState { get; }
+
+        void lockDoor();
+
+        void unlockDoor();
+
+    }
+
 }
