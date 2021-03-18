02ff8148592d5c8dba41fa04513e15546572dc14
Mahmoud-SK <mahmoudkarzoun00@gmail.com>
2021-03-18 09:12:28 +0100

    Ændrede Log fil-tekst til dansk

--- I4SWT-Group21-Ladeskab/LogFile.cs
+++ I4SWT-Group21-Ladeskab/LogFile.cs
@@ -16,14 +16,14 @@ namespace Ladeskab
 		}
 		public void LogDoorLocked(int id)
 		{
-			sb_.AppendLine("Date: " + DateTime.Now + " - Door has been locked with ID: " + id);
+			sb_.AppendLine("Date: " + DateTime.Now + " - Døren er blevet lukket med ID: " + id);
 			File.AppendAllText(Environment.CurrentDirectory + filename, sb_.ToString());
 			sb_.Clear();
 		}
 
 		public void LogDoorUnlocked(int id)
 		{
-			sb_.AppendLine("Date: " + DateTime.Now + " - Door has been unlocked with ID: " + id);
+			sb_.AppendLine("Date: " + DateTime.Now + " - Døren er låst op med ID: " + id);
 			File.AppendAllText(Environment.CurrentDirectory + filename, sb_.ToString());
 			sb_.Clear();
 		}
