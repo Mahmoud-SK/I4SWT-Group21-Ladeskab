0fa352628cb280acdb86fbd55c63d400235ef525
Mahmoud-SK <mahmoudkarzoun00@gmail.com>
2021-03-25 14:13:18 +0100

    Tilføjet et par flere tests til TestStationControl, samt nye klasser til oprettelse af mocks til LogFile og Display.

--- Ladeskab.Unit.Test/TestLogFile.cs
+++ Ladeskab.Unit.Test/TestLogFile.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Ladeskab;
+using Ladeskab.Interfaces;
 
-namespace Ladeskab.Unit.Test
+namespace LogFileTester.Test
 {
-    class TestLogFile
+    [TestFixture]
+    public class TestLogFile
     {
+        private LogFile _uut;
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new LogFile();
+        }
+
+        [TestCase(1)]
+        public void Logging_DoorUnlocked(int id)
+        {
+            _uut.LogDoorUnlocked(id);
+            
+        }
+
+        [Test]
+        public void Logging_DoorLocked(int id)
+        {
+            
+        }
     }
 }
