e9dfbb8d1239bb1068e336c8c63537ce435da360
Magn2138 <magnusgk2000@gmail.com>
2021-03-25 10:07:50 +0100

    Tilføjet tests til RfidReader

--- Ladeskab.Unit.Test/TestRfidReader.cs
+++ Ladeskab.Unit.Test/TestRfidReader.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
+using Ladeskab;
+using Ladeskab.Interfaces;
 
-namespace Ladeskab.Unit.Test
+namespace RfidRead.Test
 {
-    class TestRfidReader
-    {
-    }
+	public class TestRfidReader
+	{
+		private RfidReader _uut;
+		private RfidEventArgs _receivedEventArgs;
+
+		[SetUp]
+		public void Setup()
+		{
+			_uut = new RfidReader();
+			_receivedEventArgs = null;
+
+			_uut.RfidDetectedEvent +=
+				(o, args) =>
+				{
+					_receivedEventArgs = args;
+				};
+		}
+
+		[Test]
+		public void RfidRead_EventFired()
+		{
+			_uut.OnRfidRead(10);
+			Assert.That(_receivedEventArgs, Is.Not.Null);
+		}
+
+		[Test]
+		public void CorrectRfidRead()
+		{
+			_uut.OnRfidRead(34);
+			Assert.That(_receivedEventArgs.Id, Is.EqualTo(34));
+		}
+	}
 }
