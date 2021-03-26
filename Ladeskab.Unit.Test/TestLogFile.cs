893ce83ca93770c313b541c557635d4826bef942
Mahmoud-SK <mahmoudkarzoun00@gmail.com>
2021-03-26 16:56:25 +0100

    Changed TestLogFile.

--- Ladeskab.Unit.Test/TestLogFile.cs
+++ Ladeskab.Unit.Test/TestLogFile.cs
@@ -22,53 +22,28 @@ namespace LogFileTester.Test
             _uut = new LogFile(_fWriter, _tProvider);
         }
 
-        [TestCase(1)]
-        [TestCase(4)]
-        public void Logging_DoorUnlocked_CorrectDate(int id)
-        {
-            DateTime date = new DateTime(2021, 03, 25,15,25,13);
-            _tProvider.ProvideDate().Returns(date);
-            _uut.LogDoorUnlocked(id);
-            string resultString = "Date: " + date + " - Døren er låst op med ID: " + id;
-            _fWriter.Received(1).WriteToFile(Arg.Is<string>(x => x.Contains(date.ToString())));
-
-        }
 
         [TestCase(3)]
         [TestCase(5)]
-        public void Logging_DoorUnlocked_CorrectId(int id)
+        public void Logging_DoorUnlocked_CorrectIdAndDate(int id)
         {
             DateTime date = new DateTime(2021, 03, 25, 15, 25, 13);
             _tProvider.ProvideDate().Returns(date);
             _uut.LogDoorUnlocked(id);
-            string resultString = "Date: " + date + " - Døren er låst op med ID: " + id;
-            _fWriter.Received(1).WriteToFile(Arg.Is<string>(x => x.Contains(id.ToString())));
+            _fWriter.Received(1).WriteToFile(Arg.Is<string>(x => x.Contains("Date: " + date + " - Døren er låst op med ID: " + id)));
 
         }
-
 
         [TestCase(43)]
         [TestCase(22)]
-        public void Logging_DoorLocked_CorrectDate(int id)
+        public void Logging_DoorLocked_CorrectIdAndDate(int id)
         {
             DateTime date = new DateTime(2021, 03, 25, 15, 25, 13);
             _tProvider.ProvideDate().Returns(date);
-            _uut.LogDoorUnlocked(id);
-            string resultString = "Date: " + date + " - Døren er blevet lukket med ID: " + id;
-            _fWriter.Received(1).WriteToFile(Arg.Is<string>(x => x.Contains(date.ToString())));
+            _uut.LogDoorLocked(id);
+            _fWriter.Received(1).WriteToFile(Arg.Is<string>(x => x.Contains("Date: " + date + " - Døren er blevet lukket med ID: " + id)));
 
         }
 
-        [TestCase(43)]
-        [TestCase(333)]
-        public void Logging_DoorLocked_CorrectId(int id)
-        {
-            DateTime date = new DateTime(2021, 03, 25, 15, 25, 13);
-            _tProvider.ProvideDate().Returns(date);
-            _uut.LogDoorUnlocked(id);
-            string resultString = "Date: " + date + " - Døren er blevet lukket med ID: " + id;
-            _fWriter.Received(1).WriteToFile(Arg.Is<string>(x => x.Contains(id.ToString())));
-
-        }
     }
 }
